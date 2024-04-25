5e816f9fef460766718d51142aa7ca2cd49415f4
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-25 06:16:13 +0300

    Refactor meals.ts to include createMeal function and handle image upload

--- lib/meals.ts
+++ lib/meals.ts
@@ -1,6 +1,8 @@
+import slugify from "slugify";
+import xss from "xss";
 import sql from "better-sqlite3";
+import fs from "fs";
 const db = sql("meals.db");
-
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 3000));
   return db.prepare("SELECT * FROM meals").all();
@@ -8,5 +10,20 @@ export async function getMeals() {
 
 export async function getMeal(slug: string) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
+}
+
+export async function createMeal(meal: any) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+  const title = meal.title;
+  const extention = meal.image.name.split(".").pop();
+  const newFileName = `${slugify(title, { lower: true })}.${extention}`;
+
+  const stream = fs.createWriteStream(`public/images/${newFileName}`);
+  const bufferedStream = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedStream));
+  // const stmt = db.prepare('INSERT INTO meals (title, summary, instructions, creator, creator_email, image, slug) VALUES (@title, @summary, @instructions, @creator, @creator_email, @image, @slug)');
+  // return stmt.run(meal);
 }
